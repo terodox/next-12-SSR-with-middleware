10427f7b3834e6cc1975b182d7455255ccddc416
Fernando Dingler <ferdingler@users.noreply.github.com>
2022-08-31 16:07:06 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to fdingler!</a>
+          Welcome to fdingler!
         </h1>
 
         <p className={styles.description}>
