aced978124025151e4194c002101b574e5a0d2a8
Fernando Dingler <fdingler@amazon.com>
2022-09-01 10:26:22 -0700

    Added api that lists posts

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const json = await response.json();
+  res.status(200).json(json);
 }
